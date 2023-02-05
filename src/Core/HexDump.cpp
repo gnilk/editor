045d8fecd8979c493a05f1fc7e9d19271fed9a67
gnilk <1664069+gnilk@users.noreply.github.com>
2023-02-05 12:27:25 +0100

    NEW: HexDump functionality, good when inspecting details of strings...

---
+++ src/Core/HexDump.cpp
@@ -0,0 +1,55 @@
+//
+// Created by gnilk on 9/15/2021.
+//
+
+#include "HexDump.h"
+
+void HexDump::Write(std::function<void(const char *str)> printer, const uint8_t *pData, const size_t szData) {
+    char hexdump[16*5+2];
+    char ascii[16 + 2];
+    char* ptrHexdump = hexdump;
+    char* ptrAscii = ascii;
+    char strFinal[256];
+
+    int32_t addr = 0;
+    for (size_t i = 0; i < szData; i++) {
+        snprintf(ptrHexdump, 4, "%.2x ", pData[i]);
+        snprintf(ptrAscii, 2, "%c", ((pData[i]>31) && (pData[i]<128))?pData[i]:'.');
+        ptrAscii++;
+        ptrHexdump += 3;
+        if ((i & 15) == 15) {
+            snprintf(strFinal,sizeof(strFinal), "%.4x %s    %s", addr, hexdump, ascii);
+            printer(strFinal);
+            addr += 16;
+
+            memset(ascii, ' ', 16+2);
+            memset(hexdump, ' ', 16*5+2);
+
+            ptrHexdump = hexdump;
+            ptrAscii = ascii;
+        } else if ((i & 7) == 7) {
+            *ptrHexdump = ' ';
+            ptrHexdump++;
+        }
+    }
+
+    int nPadding = 16 - (szData & 15);
+    for (int i = 0; i < nPadding; i++) {
+        snprintf(ptrHexdump, 4, "   ");
+        ptrHexdump+= 3;
+
+    }
+    snprintf(strFinal,sizeof(strFinal), "%.4x %s     %s", addr, hexdump, ascii);
+    printer(strFinal);
+}
+
+void HexDump::ToLog(gnilk::ILogger *pLogger, const uint8_t *pData, const size_t szData) {
+    auto writer = [pLogger](const char *s){pLogger->Debug("%s",s);};
+    Write(writer, pData, szData);
+}
+
+
+void HexDump::ToConsole(const uint8_t *pData, const size_t szData) {
+    auto writer = [](const char *s){printf("%s\n",s);};
+    Write(writer, pData, szData);
+}
---
+++ src/Core/HexDump.h
@@ -0,0 +1,21 @@
+//
+// Hex dump utility class
+//
+
+#ifndef GNILK_HEXDUMP_H
+#define GNILK_HEXDUMP_H
+
+#include "logger.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <functional>
+
+class HexDump {
+public:
+    static void Write(std::function<void(const char *str)> printer, const uint8_t *pData, const size_t szData);
+    static void ToLog(gnilk::ILogger *pLogger, const uint8_t *pData, const size_t szData);
+    static void ToConsole(const uint8_t *pData, const size_t szData);
+};
+
+
+#endif //GNILK_HEXDUMP_H

e7004cc105676f184fd0ec714420ee9f544edef9
gnilk <1664069+gnilk@users.noreply.github.com>
2023-02-05 20:53:10 +0100

    Added new tokens for block-start/end operators '{' and '}' for C/CPP this allows indentlevels to be computed over a buffer for a specific line..

--- src/Core/Language/CPP/CPPLanguage.cpp
+++ src/Core/Language/CPP/CPPLanguage.cpp
@@ -6,12 +6,18 @@
 
 #include "CPPLanguage.h"
 
+
+
 // state: main (and probably a few others)
 static const std::string cppTypes = "void int char";
 static const std::string cppKeywords = "auto typedef class struct static enum for while if return const";
+// FIXME: Remove '{' and '}' from here - add separatley with own identifiers, this way we can parse the meta data and
+//        count block-start/ends in order to properly compute indentation...
 // Note: Multi char operators must be declared first...
-static const std::string cppOperators = "== ++ -- << >> += -= *= /= = + - < > ( , * ) { [ ] } < > ; ' \"";
+static const std::string cppOperators = "== ++ -- << >> += -= *= /= = + - < > ( , * ) [ ] < > ; ' \"";
 static const std::string cppLineComment = "//";
+static const std::string cppCodeBlockStart = "{";
+static const std::string cppCodeBlockEnd = "}";
 // state: main & in_block_comment
 static const std::string cppBlockCommentStart = "/* */";
 static const std::string cppBlockCommentStop = "*/";
@@ -25,11 +31,13 @@ static const std::string inStringPostFixOp = "\"";
 //
 bool CPPLanguage::Initialize() {
     auto state = tokenizer.GetOrAddState("main");
-    state->SetIdentifiers(gnilk::kLanguageTokenClass::kOperator, cppOperators.c_str());
-    state->SetIdentifiers(gnilk::kLanguageTokenClass::kKeyword, cppKeywords.c_str());
-    state->SetIdentifiers(gnilk::kLanguageTokenClass::kKnownType, cppTypes.c_str());
-    state->SetIdentifiers(gnilk::kLanguageTokenClass::kLineComment, cppLineComment.c_str());
-    state->SetIdentifiers(gnilk::kLanguageTokenClass::kBlockComment, cppBlockCommentStart.c_str());
+    state->SetIdentifiers(kLanguageTokenClass::kOperator, cppOperators.c_str());
+    state->SetIdentifiers(kLanguageTokenClass::kKeyword, cppKeywords.c_str());
+    state->SetIdentifiers(kLanguageTokenClass::kKnownType, cppTypes.c_str());
+    state->SetIdentifiers(kLanguageTokenClass::kLineComment, cppLineComment.c_str());
+    state->SetIdentifiers(kLanguageTokenClass::kBlockComment, cppBlockCommentStart.c_str());
+    state->SetIdentifiers(kLanguageTokenClass::kCodeBlockStart, cppCodeBlockStart.c_str());
+    state->SetIdentifiers(kLanguageTokenClass::kCodeBlockEnd, cppCodeBlockEnd.c_str());
     state->SetPostFixIdentifiers(cppOperators.c_str());
 
     state->GetOrAddAction("\"",gnilk::LangLineTokenizer::kAction::kPushState, "in_string");
@@ -37,21 +45,21 @@ bool CPPLanguage::Initialize() {
     state->GetOrAddAction("//", gnilk::LangLineTokenizer::kAction::kPushState, "in_line_comment");
 
     auto stateStr = tokenizer.GetOrAddState("in_string");
-    stateStr->SetRegularTokenClass(gnilk::kLanguageTokenClass::kString);
-    stateStr->SetIdentifiers(gnilk::kLanguageTokenClass::kString, inStringOperators.c_str());
+    stateStr->SetRegularTokenClass(kLanguageTokenClass::kString);
+    stateStr->SetIdentifiers(kLanguageTokenClass::kString, inStringOperators.c_str());
     stateStr->SetPostFixIdentifiers(inStringPostFixOp.c_str());
     stateStr->GetOrAddAction("\"", gnilk::LangLineTokenizer::kAction::kPopState);
 
     auto stateBlkComment = tokenizer.GetOrAddState("in_block_comment");
     // just testing, kFunky should be reclassified to 'kBlockComment' once this state is popped...
-    stateBlkComment->SetIdentifiers(gnilk::kLanguageTokenClass::kBlockComment, cppBlockCommentStop.c_str());
+    stateBlkComment->SetIdentifiers(kLanguageTokenClass::kBlockComment, cppBlockCommentStop.c_str());
     stateBlkComment->SetPostFixIdentifiers(cppBlockCommentStop.c_str());
     stateBlkComment->GetOrAddAction("*/", gnilk::LangLineTokenizer::kAction::kPopState);
-    stateBlkComment->SetRegularTokenClass(gnilk::kLanguageTokenClass::kCommentedText);
+    stateBlkComment->SetRegularTokenClass(kLanguageTokenClass::kCommentedText);
 
     // a line comment run's to new-line...
     auto stateLineComment = tokenizer.GetOrAddState("in_line_comment");
-    stateLineComment->SetRegularTokenClass(gnilk::kLanguageTokenClass::kCommentedText);
+    stateLineComment->SetRegularTokenClass(kLanguageTokenClass::kCommentedText);
     stateLineComment->SetEOLAction(gnilk::LangLineTokenizer::kAction::kPopState);
 
 

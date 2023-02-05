4b3ab3494aa9ed3f1f71105318bc384c43ef6834
gnilk <1664069+gnilk@users.noreply.github.com>
2023-02-05 09:44:58 +0100

    small test of logger functionality

---
+++ tests/tstlog.cpp
@@ -0,0 +1,19 @@
+//
+// Created by gnilk on 05.02.23.
+//
+#include "logger.h"
+static void SetupLogger() {
+    char *sinkArgv[]={"file","logfile.log"};
+    auto fileSink = new gnilk::LogFileSink();
+    gnilk::Logger::AddSink(fileSink, "fileSink", 2, sinkArgv);
+    auto logger = gnilk::Logger::GetLogger("main");
+}
+
+int main(int argc, char **argv) {
+    SetupLogger();
+    auto logger = gnilk::Logger::GetLogger("main");
+    logger->Debug("Test");
+    auto log2 = gnilk::Logger::GetLogger("apa");
+    log2->Debug("Test2");
+    return -1;
+}

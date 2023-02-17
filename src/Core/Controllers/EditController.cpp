2f894dd7fff8c02e40e403ebd61cb6524fc46c56
gnilk <1664069+gnilk@users.noreply.github.com>
2023-02-17 09:52:15 +0100

    CHG: Do not handle (as of yet) any modifier keys

--- src/Core/Controllers/EditController.cpp
+++ src/Core/Controllers/EditController.cpp
@@ -22,6 +22,9 @@ bool EditController::HandleKeyPress(Cursor &cursor, size_t idxLine, const gedit:
     if (!textBuffer) {
         return false;
     }
+    // FIXME - not quite true, but as long as we don't deal with it yet....
+    if (keyPress.modifiers) return  false;
+
     auto line = textBuffer->LineAt(idxLine);
     if (DefaultEditLine(cursor, line, keyPress)) {
         return true;

4d4567f5dbf3661f6745bc7da3bc6ce9e1033cf5
gnilk <1664069+gnilk@users.noreply.github.com>
2023-03-18 11:41:05 +0100

    CHG: Removed the header drawing to own class

--- src/Core/Views/SingleLineView.h
+++ src/Core/Views/SingleLineView.h
@@ -34,26 +34,6 @@ namespace gedit {
             viewRect.SetHeight(1);
             window = screen->UpdateWindow(window, viewRect, WindowBase::kWin_Visible, WindowBase::kWinDeco_None);
         }
-    protected:
-        void DrawViewContents() override {
-            auto &dc = window->GetContentDC();
-            auto &models = Editor::Instance().GetModels();
-            // FIXME: should have attribute...
-            std::string header = "Files:>| ";
-            for(auto &m : models) {
-                auto &name = m->GetTextBuffer()->Name();
-
-                if (m->IsActive()) {
-                    header += "! ";
-                }
-
-                header += name;
-                header += " | ";
-            }
-            //dc.DrawStringAt(0,0,"File1.txt | File2.txt | File3.txt                 - wefwef");
-            dc.DrawStringAt(0,0,header.c_str());
-        }
-
     };
 }
 

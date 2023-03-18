4eb3808c1960a992f863ec164672b8ce686b9957
gnilk <1664069+gnilk@users.noreply.github.com>
2023-03-18 11:40:05 +0100

    CHG: Specialized HSplitter which draws a statusbar in the splitter area

---
+++ src/Core/Views/HSplitViewStatus.h
@@ -0,0 +1,55 @@
+//
+// Created by gnilk on 18.03.23.
+//
+
+#ifndef EDITOR_HSPLITVIEWSTATUS_H
+#define EDITOR_HSPLITVIEWSTATUS_H
+#include "HSplitView.h"
+#include "Core/RuntimeConfig.h"
+
+namespace gedit {
+    class HSplitViewStatus : public HSplitView {
+    public:
+        HSplitViewStatus() = default;
+        explicit HSplitViewStatus(const Rect &rect) : HSplitView(rect) {
+
+        }
+        virtual ~HSplitViewStatus() = default;
+
+    protected:
+        void DrawSplitter(int row) override {
+            auto &dc = window->GetContentDC();
+
+            auto logger = gnilk::Logger::GetLogger("HSplitViewStatus");
+            logger->Debug("DrawSplitter, row=%d, height=%d", row, dc.GetRect().Height());
+
+            auto model = RuntimeConfig::Instance().ActiveEditorModel();
+            if (model == nullptr) {
+                logger->Error("model is nullptr!");
+                exit(1);
+            }
+            //model->cursor.position.x
+            std::string dummy(dc.GetRect().Width(), ' ');
+            std::string statusLine = " GoatEdit V0.1 | ";
+            statusLine += model->GetTextBuffer()->Name();
+
+            statusLine += " | ";
+            statusLine += model->GetTextBuffer()->HaveLanguage()?model->GetTextBuffer()->LangParser().Identifier():"none";
+
+            dc.FillLine(row, kTextAttributes::kInverted, ' ');
+            dc.DrawStringWithAttributesAt(0,row, kTextAttributes::kInverted, statusLine.c_str());
+
+            statusLine = "";
+            char tmp[32];
+            snprintf(tmp,32, "Ln: %d, Col: %d", model->cursor.position.y, model->cursor.position.x);
+            statusLine += tmp;
+
+            dc.DrawStringWithAttributesAt(dc.GetRect().Width()-statusLine.size()-4,row, kTextAttributes::kInverted, statusLine.c_str());
+
+
+
+        }
+    };
+}
+
+#endif //EDITOR_HSPLITVIEWSTATUS_H

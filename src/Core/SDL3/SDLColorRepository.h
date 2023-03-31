1b9ff64a4e2c11a7585377b30a77890ca56a3875
gnilk <1664069+gnilk@users.noreply.github.com>
2023-03-31 11:19:47 +0200

    CHG: SDL Color Repository is now self initialized
    It reads and initializes the colors needed by the SDL3 rendering. This was previously done in Screen but it makes more sense having it here..

--- src/Core/SDL3/SDLColorRepository.h
+++ src/Core/SDL3/SDLColorRepository.h
@@ -9,7 +9,10 @@
 #include <utility>
 #include <SDL3/SDL.h>
 #include "Core/ColorRGBA.h"
+#include "Core/Config/Config.h"
 #include "SDLColor.h"
+
+#include "logger.h"
 
 
 namespace gedit {
@@ -19,8 +22,10 @@ namespace gedit {
         virtual ~SDLColorRepository() = default;
         static SDLColorRepository &Instance() {
             static SDLColorRepository glbColorRepository;
+            glbColorRepository.Initialize();
             return glbColorRepository;
         }
+
 
         void RegisterColor(int appIndex, const ColorRGBA &fg, const ColorRGBA &bg) {
             colors[appIndex]=std::make_pair(fg,bg);
@@ -52,9 +57,44 @@ namespace gedit {
             backgroundColor.Use(renderer);
         }
 
+        void SetCursorColor(const SDLColor &newCursorColor) {
+            cursorColor = newCursorColor;
+        }
+
+        void UseCursorColor(SDL_Renderer *renderer) {
+            cursorColor.Use(renderer);
+        }
+    protected:
+        void Initialize() {
+            if (isInitialized) {
+                return;
+            }
+            auto logger = gnilk::Logger::GetLogger("SDLColorRepo");
+            if (!Config::Instance().ColorConfiguration().HasColor("background")) {
+                logger->Warning("No background color found - assigning default");
+                // Auto assign background here..
+                backgroundColor = SDLColor(ColorRGBA::FromRGBA(46, 54, 62, 255));
+            } else {
+                backgroundColor = SDLColor(Config::Instance().ColorConfiguration().GetColor("background"));
+            }
+
+
+            if (!Config::Instance().ColorConfiguration().HasColor("caret")) {
+                logger->Warning("No cursor color found - assigning default");
+                // Auto assign background here..
+                cursorColor = SDLColor(ColorRGBA::FromRGBA(46, 54, 62, 255));
+            } else {
+                cursorColor = SDLColor(Config::Instance().ColorConfiguration().GetColor("caret"));
+            }
+        }
+
     private:
         SDLColorRepository() = default;
+    private:
+        bool isInitialized = false;
         SDLColor backgroundColor;   // Special for background
+        SDLColor cursorColor;       // Special for cursor
+
         // These are the original colors
         std::map<int, std::pair<ColorRGBA, ColorRGBA> > colors;
         // We also pre convert directly here

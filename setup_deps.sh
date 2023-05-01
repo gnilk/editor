737da65fcfdf824ca97f95101fc3f89fade16b48
gnilk <1664069+gnilk@users.noreply.github.com>
2023-05-01 16:50:36 +0200

    Added DukTape JS Engine and DukGlue for CPP bindings

--- setup_deps.sh
+++ setup_deps.sh
@@ -5,6 +5,7 @@ git clone https://github.com/gnilk/dukglue ext/dukglue
 git clone https://github.com/gnilk/logger ext/logger
 
 # clone specifically branch 2.7.0 of duktape
+# NOTE: You need to run the configure script first!!!
 git clone -b v2.7.0 https://github.com/svaarala/duktape ext/duktape-v2.7.0
 
 #

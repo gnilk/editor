f0233676731da567f0fff6f998e64dc20f1e0443
gnilk <1664069+gnilk@users.noreply.github.com>
2023-05-04 17:26:46 +0200

    NEW: Console API for JS Engine
    Support for variadic arguments

---
+++ src/Core/JSEngine/Modules/ConsoleAPIWrapper.cpp
@@ -0,0 +1,48 @@
+//
+// Created by gnilk on 04.05.23.
+//
+
+#include "dukglue/dukglue.h"
+
+#include "ConsoleAPIWrapper.h"
+
+using namespace gedit;
+
+// Do variadic argument printing - this is pretty much lifted directly from duktape/extras/console
+// I do understand (to some extent) what it is doing, but I wouldn't have been able to write it myself...
+duk_ret_t ConsoleAPIWrapper::WriteLine(duk_context *ctx) {
+    duk_uint_t flags = (duk_uint_t) duk_get_current_magic(ctx);
+
+    duk_idx_t n = duk_get_top(ctx);
+    duk_idx_t i;
+
+    for (i = 0; i < n; i++) {
+        if (duk_check_type_mask(ctx, i, DUK_TYPE_MASK_OBJECT)) {
+            /* Slow path formatting. */
+            duk_dup(ctx, -1);  /* console.format */
+            duk_dup(ctx, i);
+            duk_call(ctx, 1);
+            duk_replace(ctx, i);  /* arg[i] = console.format(arg[i]); */
+        }
+    }
+
+    duk_push_string(ctx, " ");
+    duk_insert(ctx, 0);
+    duk_join(ctx, n);
+
+    // NOTE: This should NOT go here!
+    fprintf(stdout, "%s\n", duk_to_string(ctx, -1));
+    fflush(stdout);
+    return 0;
+}
+
+void ConsoleAPIWrapper::RegisterModule(duk_context *ctx) {
+    static ConsoleAPIWrapper consoleApiWrapper;
+
+    dukglue_push(ctx, &consoleApiWrapper);
+    duk_put_global_string(ctx, "Console");
+
+    dukglue_register_method_varargs(ctx,&ConsoleAPIWrapper::WriteLine, "WriteLine");
+    dukglue_register_method_varargs(ctx,&ConsoleAPIWrapper::WriteLine, "log");
+}
+
---
+++ src/Core/JSEngine/Modules/ConsoleAPIWrapper.h
@@ -0,0 +1,19 @@
+//
+// Created by gnilk on 04.05.23.
+//
+
+#ifndef EDITOR_CONSOLEAPIWRAPPER_H
+#define EDITOR_CONSOLEAPIWRAPPER_H
+
+#include "duktape.h"
+
+namespace gedit {
+    class ConsoleAPIWrapper {
+    public:
+        duk_ret_t WriteLine(duk_context *ctx);
+        static void RegisterModule(duk_context *ctx);
+    };
+}
+
+
+#endif //EDITOR_CONSOLEAPIWRAPPER_H

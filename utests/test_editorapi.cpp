c8a9f544c82a545d66dcb6911f833aa67228b621
gnilk <1664069+gnilk@users.noreply.github.com>
2023-05-05 22:06:03 +0200

    NEW: EditorAPI unit test

---
+++ utests/test_editorapi.cpp
@@ -0,0 +1,28 @@
+//
+// Created by gnilk on 05.05.23.
+//
+#include <testinterface.h>
+#include "Core/Editor.h"
+#include "Core/API/EditorAPI.h"
+
+
+using namespace gedit;
+
+extern "C" {
+DLL_EXPORT int test_editorapi(ITesting *t);
+DLL_EXPORT int test_editorapi_listlang(ITesting *t);
+}
+DLL_EXPORT int test_editorapi(ITesting *t) {
+    return kTR_Pass;
+}
+DLL_EXPORT int test_editorapi_listlang(ITesting *t) {
+    auto editorApi = Editor::Instance().GetGlobalAPIObject<EditorAPI>();
+    auto v = editorApi->GetRegisteredLanguages();
+    TR_ASSERT(t, v.size() != 0);
+    for(auto &l : v) {
+        printf("%s\n",l.c_str());
+    }
+    return kTR_Pass;
+}
+
+

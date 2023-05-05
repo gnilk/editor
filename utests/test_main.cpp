01b48b4a4678b7719dd63a9dff4753c3c898210e
gnilk <1664069+gnilk@users.noreply.github.com>
2023-05-05 22:06:25 +0200

    NEW: Setting up IOutputConsole for unit testing
    Make the default IOutputConsole point stdout

--- utests/test_main.cpp
+++ utests/test_main.cpp
@@ -4,6 +4,7 @@
 #include <testinterface.h>
 #include "logger.h"
 #include "Core/Editor.h"
+#include "Core/RuntimeConfig.h"
 
 using namespace gedit;
 
@@ -11,8 +12,20 @@ extern "C" {
     DLL_EXPORT int test_main(ITesting *t);
 }
 
+class UTestConsole : public IOutputConsole {
+public:
+    void WriteLine(const std::string &str) override;
+};
+static UTestConsole utestConsole;
+
+void UTestConsole::WriteLine(const std::string &str) {
+    fprintf(stdout, "%s\n", str.c_str());
+    fflush(stdout);
+}
+
 DLL_EXPORT int test_main(ITesting *t) {
     // We need this..
     Editor::Instance().Initialize(0, nullptr);
+    RuntimeConfig::Instance().SetOutputConsole(&utestConsole);
     return kTR_Pass;
 }

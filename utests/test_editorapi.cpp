5c87c09608e593f4ea4e03c78fddc898495a5be8
gnilk <1664069+gnilk@users.noreply.github.com>
2023-05-06 23:03:52 +0200

    NEW: test new loadbuffer api

--- utests/test_editorapi.cpp
+++ utests/test_editorapi.cpp
@@ -12,6 +12,7 @@ extern "C" {
 DLL_EXPORT int test_editorapi(ITesting *t);
 DLL_EXPORT int test_editorapi_listlang(ITesting *t);
 DLL_EXPORT int test_editorapi_newbuffer(ITesting *t);
+DLL_EXPORT int test_editorapi_loadbuffer(ITesting *t);
 }
 DLL_EXPORT int test_editorapi(ITesting *t) {
     return kTR_Pass;
@@ -33,5 +34,14 @@ DLL_EXPORT int test_editorapi_newbuffer(ITesting *t) {
     auto numAfter = Editor::Instance().GetModels().size();
     TR_ASSERT(t, numAfter > numBefore);
 
+    return kTR_Pass;
+}
+
+DLL_EXPORT int test_editorapi_loadbuffer(ITesting *t) {
+    auto editorApi = Editor::Instance().GetGlobalAPIObject<EditorAPI>();
+    auto numBefore = Editor::Instance().GetModels().size();
+    editorApi->LoadBuffer("example.json");
+    auto numAfter = Editor::Instance().GetModels().size();
+    TR_ASSERT(t, numAfter > numBefore);
     return kTR_Pass;
 }

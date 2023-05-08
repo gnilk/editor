ce0c68f567aac5e3bafef530f6d648bf9a8bc3d0
gnilk <1664069+gnilk@users.noreply.github.com>
2023-05-08 07:36:51 +0200

    FIX: Removed dead code

--- src/Core/Controllers/CommandController.cpp
+++ src/Core/Controllers/CommandController.cpp
@@ -31,13 +31,8 @@ void CommandController::Begin() {
 }
 
 void CommandController::NewLine() {
-    if (currentLine != nullptr) {
-        //currentLine->SetActive(false);
-    }
     std::lock_guard<std::mutex> guard(lineLock);
     currentLine = std::make_shared<Line>();
-    //currentLine->SetActive(true);
-//    historyBuffer.push_back(currentLine);
     if (onNewLine != nullptr) {
         onNewLine();
     }

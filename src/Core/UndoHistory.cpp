e5061d2d8a39a100e66e12975a7ac894d4e5984e
gnilk <1664069+gnilk@users.noreply.github.com>
2023-07-18 17:43:04 +0200

    NEW: Undo handling for multi-lines or 'ranges'

--- src/Core/UndoHistory.cpp
+++ src/Core/UndoHistory.cpp
@@ -8,15 +8,102 @@
 using namespace gedit;
 
 UndoHistory::UndoItem::Ref UndoHistory::NewUndoItem() {
-    auto undoItem = std::make_shared<UndoHistory::UndoItem>();
+    auto undoItem = UndoItemSingle::Create();
+    undoItem->Initialize();
+    return undoItem;
+}
+
+UndoHistory::UndoItem::Ref UndoHistory::NewUndoFromSelection() {
+    auto undoItem = UndoItemRange::Create();
     auto model = Editor::Instance().GetActiveModel();
     if (model == nullptr) {
         return undoItem;
     }
-    undoItem->idxLine = model->idxActiveLine;
-    undoItem->offset = model->cursor.position.x;
-    auto line = model->LineAt(undoItem->idxLine);
-    undoItem->data = line->Buffer();    // We are saving the "complete" previous line
+    if (!model->IsSelectionActive()) {
+        return undoItem;
+    }
+    auto selection = model->GetSelection();
+
+    undoItem->InitRange(selection.GetStart(), selection.GetEnd());
     return undoItem;
 }
+
+
+void UndoHistory::RestoreOneItem(Cursor &cursor, TextBuffer::Ref textBuffer) {
+    auto undoItem = PopItem();
+    // Unless we have been initialized properly, we won't restore...
+    if (!undoItem->IsValid()) {
+        return;
+    }
+    undoItem->Restore(textBuffer);
+    // Update cursor
+    cursor.position.x = undoItem->offset;
+    cursor.wantedColumn = undoItem->offset;
+}
+
+//////////////////
+// Baseclass
+void UndoHistory::UndoItem::Initialize() {
+    auto model = Editor::Instance().GetActiveModel();
+    if (model == nullptr) {
+        return;
+    }
+    idxLine = model->idxActiveLine;
+    offset = model->cursor.position.x;
+    isValid = true;
+}
+
+// Single Undo item
+UndoHistory::UndoItemSingle::Ref UndoHistory::UndoItemSingle::Create() {
+    auto undoItem = std::make_shared<UndoHistory::UndoItemSingle>();
+    return undoItem;
+}
+
+void UndoHistory::UndoItemSingle::Initialize() {
+
+    UndoHistory::UndoItem::Initialize();
+
+    auto model = Editor::Instance().GetActiveModel();
+    if (model == nullptr) {
+        return;
+    }
+    auto line = model->LineAt(idxLine);
+    data = line->Buffer();    // We are saving the "complete" previous line
+}
+
+
+void UndoHistory::UndoItemSingle::Restore(TextBuffer::Ref textBuffer) {
+    auto line = textBuffer->LineAt(idxLine);
+    line->Clear();
+    line->Append(data);
+}
+
+// Range based undo item
+UndoHistory::UndoItemRange::Ref UndoHistory::UndoItemRange::Create() {
+    auto undoItem = std::make_shared<UndoHistory::UndoItemRange>();
+    return undoItem;
+}
+void UndoHistory::UndoItemRange::InitRange(const gedit::Point &ptStart, const gedit::Point &ptEnd) {
+    UndoHistory::UndoItem::Initialize();
+
+    auto model = Editor::Instance().GetActiveModel();
+    if (model == nullptr) {
+        return;
+    }
+
+    start = ptStart;
+    end = ptEnd;
+
+    for(int y=start.y; y < end.y; y++) {
+        auto line = model->LineAt(y);
+        data.push_back(line);
+    }
+}
+void UndoHistory::UndoItemRange::Restore(TextBuffer::Ref textBuffer) {
+    int lineCounter = 0;
+    for(int y=start.y; y<end.y;y++) {
+        textBuffer->Insert(start.y, data[lineCounter++]);
+    }
+}
+
 
--- src/Core/UndoHistory.h
+++ src/Core/UndoHistory.h
@@ -11,24 +11,69 @@
 #include <utility>
 
 #include "logger.h"
-
+#include "TextBuffer.h"
 namespace gedit {
     // Extremely simplistic undo buffer - works for regular edits but wastes an extreme amount of memory..
     class UndoHistory {
     public:
+        // Base class for any item which can be un-done
+        // holds common data
         class UndoItem {
+            friend UndoHistory;
         public:
             using Ref = std::shared_ptr<UndoItem>;
         public:
+            UndoItem() = default;
+            virtual ~UndoItem() = default;
+            virtual void Restore(TextBuffer::Ref textBuffer) {}
+            bool IsValid() { return isValid; }
+        protected:
+            virtual void Initialize();
+        protected:
+            bool isValid = false;
             int idxLine = {};
             int offset = {};
             int action = {};
+        };
+
+        // Specialization for a single item (line) which can be un-done, this one holds the actual data
+        class UndoItemSingle : public UndoItem {
+            friend UndoHistory;
+        public:
+            using Ref = std::shared_ptr<UndoItemSingle>;
+        public:
+            UndoItemSingle() = default;
+            virtual ~UndoItemSingle() = default;
+            static UndoItemSingle::Ref Create();
+            void Restore(TextBuffer::Ref buffer) override;
+        protected:
+            void Initialize() override;
+        private:
             std::string data = {};
         };
+        class UndoItemRange : public UndoItem {
+            friend UndoHistory;
+        public:
+            using Ref = std::shared_ptr<UndoItemRange>;
+        public:
+            UndoItemRange() = default;
+            virtual ~UndoItemRange() = default;
+
+            static UndoItemRange::Ref Create();
+            void Restore(TextBuffer::Ref textBuffer) override;
+         protected:
+            void InitRange(const Point &ptStart, const Point &ptEnd);
+        private:
+            Point start = {};
+            Point end = {};
+            std::vector<Line::Ref> data;
+        };
+
     public:
         UndoHistory() = default;
         virtual ~UndoHistory() = default;
         UndoItem::Ref NewUndoItem();
+        UndoItem::Ref NewUndoFromSelection();
         void PushUndoItem(UndoItem::Ref undoItem) {
             historystack.push_front(undoItem);
         }
@@ -37,6 +82,8 @@ namespace gedit {
             historystack.pop_front();
             return top;
         }
+        void RestoreOneItem(Cursor &cursor, TextBuffer::Ref textBuffer);
+
         bool HaveHistory() {
             return !historystack.empty();
         }
@@ -44,7 +91,7 @@ namespace gedit {
             auto logger = gnilk::Logger::GetLogger("History");
             int count = 0;
             for(auto &undoItem : historystack) {
-                logger->Debug("%d: line=%d, offset=%d, data=%s", count, undoItem->idxLine, undoItem->offset, undoItem->data.c_str());
+                logger->Debug("%d: line=%d, offset=%d", count, undoItem->idxLine, undoItem->offset);
                 count ++;
             }
         }

cad6a1ec1469d736d34ebf97bdcc9af55e125920
gnilk <1664069+gnilk@users.noreply.github.com>
2023-07-18 10:49:05 +0200

    NEW: Unit tests for cpp parser
    Found an error in the language tokenizer - was off by one in indent calculation

---
+++ utests/test_cpplang.cpp
@@ -0,0 +1,45 @@
+//
+// Created by gnilk on 18.07.23.
+//
+//
+// Created by gnilk on 05.05.23.
+//
+#include <testinterface.h>
+#include "Core/Editor.h"
+#include "Core/EditorModel.h"
+#include "Core/Language/LanguageBase.h"
+#include "Core/Language/LangLineTokenizer.h"
+#include "Core/TextBuffer.h"
+
+
+using namespace gedit;
+
+extern "C" {
+DLL_EXPORT int test_cpplang(ITesting *t);
+DLL_EXPORT int test_cpplang_indent(ITesting *t);
+}
+
+DLL_EXPORT int test_cpplang(ITesting *t) {
+    return kTR_Pass;
+}
+DLL_EXPORT int test_cpplang_indent(ITesting *t) {
+    auto model = Editor::Instance().NewModel("test.cpp");
+    auto buffer = model->GetTextBuffer();
+
+    buffer->AddLine("if (a==b) {");
+    buffer->AddLine("a");
+    buffer->AddLine("b");
+    buffer->AddLine("if (c==d) {");
+    buffer->AddLine("c");
+    buffer->AddLine("}");
+    buffer->AddLine("}");
+
+    buffer->Reparse();
+
+    for(int i=0;i<buffer->NumLines();i++) {
+        auto line = buffer->LineAt(i);
+        printf("%d: indent: %d - data: %s\n", i, line->Indent(), line->Buffer().data());
+    }
+
+    return kTR_Pass;
+}

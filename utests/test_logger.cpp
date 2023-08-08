0b51313b7096ac5ffe55ce0b517a488edeb31cc1
gnilk <1664069+gnilk@users.noreply.github.com>
2023-08-08 15:13:27 +0200

    NEW: Test to check dual remove of sinks

--- utests/test_logger.cpp
+++ utests/test_logger.cpp
@@ -8,6 +8,7 @@
 extern "C" {
 DLL_EXPORT int test_logger(ITesting *t);
 DLL_EXPORT int test_logger_debug(ITesting *t);
+DLL_EXPORT int test_logger_removesink(ITesting *t);
 }
 
 class MockSink : public gnilk::LogBaseSink {
@@ -32,26 +33,35 @@ DLL_EXPORT int test_logger(ITesting *t) {
     return kTR_Pass;
 }
 DLL_EXPORT int test_logger_debug(ITesting *t) {
+
+    gnilk::Logger::EnableAllLoggers();
+
     auto logger = gnilk::Logger::GetLogger("test");
     auto logger2 = gnilk::Logger::GetLogger("test2");
-    MockSink sink;
-    gnilk::Logger::AddSink(&sink, "mock");
-    TR_ASSERT(t, sink.GetCounter() == 0);
+    MockSink *sink = new MockSink();
+    gnilk::Logger::AddSink(sink, "mock");
+    TR_ASSERT(t, sink->GetCounter() == 0);
     logger->Debug("wefwef");
-    TR_ASSERT(t, sink.GetCounter() == 1);
+    TR_ASSERT(t, sink->GetCounter() == 1);
     logger->SetEnabled(false);
     logger->Debug("wefwef");
-    TR_ASSERT(t, sink.GetCounter() == 1);
+    TR_ASSERT(t, sink->GetCounter() == 1);
 
     gnilk::Logger::DisableAllLoggers();
     logger->Debug("wefwef");
     logger2->Debug("wefwfe");
-    TR_ASSERT(t, sink.GetCounter() == 1);
+    TR_ASSERT(t, sink->GetCounter() == 1);
 
     gnilk::Logger::EnableLogger("test2");
     logger->Debug("wefwef");
     logger2->Debug("wefwfe");
-    TR_ASSERT(t, sink.GetCounter() == 2);
+    TR_ASSERT(t, sink->GetCounter() == 2);
 
+    return kTR_Pass;
+}
+DLL_EXPORT int test_logger_removesink(ITesting *t) {
+    printf("sinksize: %d\n", (int)gnilk::Logger::sinks.size());
+    gnilk::Logger::RemoveSink("console");
+    gnilk::Logger::RemoveSink("console");
     return kTR_Pass;
 }

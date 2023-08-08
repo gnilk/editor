a357735f4f41b6a92880952f643f90990b84230d
gnilk <1664069+gnilk@users.noreply.github.com>
2023-08-08 17:50:12 +0200

    Added a bit more text than just the goat ascii art

--- src/Plugins/Scripts/hello.js
+++ src/Plugins/Scripts/hello.js
@@ -26,5 +26,7 @@ function main(args) {
     Console.WriteLine("                                      &(,,*(%              (,..,*///(%          ");
     Console.WriteLine("                                    %*.*/*,,*/#&        %/...,*/*,,*///#&       ");
     Console.WriteLine("                                    &#////////#&        %*.,/*,,*//////#&       ");
-
+    Console.WriteLine("                                                                                ");
+    Console.WriteLine("    GoatEdit v0.1.0 - by Gnilk - a simple small editor                          ");
+    Console.WriteLine("    (c) 2023 Gnilk, see: https://github.com/gnilk/editor                        ");
 }

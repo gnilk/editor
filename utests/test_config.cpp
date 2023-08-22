6582d759b496c068e3d4e466d438ce1fa10b442f
gnilk <1664069+gnilk@users.noreply.github.com>
2023-08-22 23:01:34 +0200

    NEW: Unittest for templated config get/set

--- utests/test_config.cpp
+++ utests/test_config.cpp
@@ -10,6 +10,7 @@ using namespace gedit;
 extern "C" {
 DLL_EXPORT int test_config(ITesting *t);
 DLL_EXPORT int test_config_setint(ITesting *t);
+DLL_EXPORT int test_config_setdyn(ITesting *t);
 }
 
 DLL_EXPORT int test_config(ITesting *t) {
@@ -26,5 +27,24 @@ DLL_EXPORT int test_config_setint(ITesting *t) {
 
     auto s = node.ToString();
     printf("str: %s\n", s.c_str());
+    return kTR_Pass;
+}
+
+enum kDummyTest : int {
+    kApa = 1,
+    kBpa = 2,
+};
+DLL_EXPORT int test_config_setdyn(ITesting *t) {
+    ConfigNode node;
+
+    node.SetValue<int>("bla",2);
+    TR_ASSERT(t, node.GetInt("bla", -1) == 2);
+
+    node.SetValue<std::string>("bla2","hello");
+    TR_ASSERT(t, node.GetStr("bla2", "wef") == "hello");
+
+    node.SetValue<int>("enum", kDummyTest::kApa);
+    TR_ASSERT(t, node.GetValue<int>("enum", -1) == kDummyTest::kApa);
+
     return kTR_Pass;
 }

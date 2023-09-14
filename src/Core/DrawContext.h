b317bc1c925853d56a4ba33415cede0ae85beae5
gnilk <1664069+gnilk@users.noreply.github.com>
2023-09-14 13:27:37 +0200

    NEW: DrawContext base class now provides prototypes for drawing u32string's

--- src/Core/DrawContext.h
+++ src/Core/DrawContext.h
@@ -90,8 +90,6 @@ namespace gedit {
 
         virtual void DrawStringAt(int x, int y, const std::u32string &str) const {}
         virtual void DrawStringWithAttributesAt(int x, int y, kTextAttributes attrib, const std::u32string &str) const {}
-        virtual void DrawStringWithAttributesAndColAt(int x, int y, kTextAttributes attrib, int idxColor, const std::u32string &str) const {}
-
 
         __inline void SetBGColor(const ColorRGBA &newBackgroundColor) {
             const_cast<DrawContext *>(this)->bgColor = newBackgroundColor;

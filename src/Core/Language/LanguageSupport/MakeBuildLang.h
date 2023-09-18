3e35706f24d4d6bd105e23fb09c95703b0fa5fa4
gnilk <1664069+gnilk@users.noreply.github.com>
2023-09-18 22:40:59 +0200

    NEW: Testing language parser for makefile output

---
+++ src/Core/Language/LanguageSupport/MakeBuildLang.h
@@ -0,0 +1,34 @@
+//
+// Created by gnilk on 18.09.23.
+//
+
+#ifndef GOATEDIT_MAKEBUILDLANG_H
+#define GOATEDIT_MAKEBUILDLANG_H
+
+#include <memory>
+#include "Core/Language/LanguageBase.h"
+
+namespace gedit {
+    // This is a test to see if I can use the language parser to parse the output from 'make' and similar..
+    class MakeBuildLang : public LanguageBase {
+    public:
+        MakeBuildLang() = default;
+        virtual ~MakeBuildLang() = default;
+
+        static LanguageBase::Ref Create() {
+            return std::make_shared<MakeBuildLang>();
+        }
+
+        bool Initialize() override;
+        void OnPostProcessParsedLine(Line::Ref line) override;
+
+        const std::u32string &Identifier() override {
+            static std::u32string cppIdentifier = U"make";
+            return cppIdentifier;
+        }
+
+    };
+}
+
+
+#endif //GOATEDIT_MAKEBUILDLANG_H

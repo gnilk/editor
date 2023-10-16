2d89333aee26ad7f668ebfa77b6d7f2fa71e322c
gnilk <1664069+gnilk@users.noreply.github.com>
2023-10-16 17:15:58 +0200

    FIX: Unit test for timer

--- utests/test_timer.cpp
+++ utests/test_timer.cpp
@@ -22,14 +22,29 @@ DLL_EXPORT int test_timer(ITesting *t) {
 }
 
 DLL_EXPORT int test_timer_exit(ITesting *t) {
-    TimerController::Instance().Stop();
     return kTR_Pass;
 }
 
 DLL_EXPORT int test_timer_create(ITesting *t) {
-    auto timer = Timer::Create(1000ms, [](){
-        printf("Expired\n");
+    TimerController timerController;
+    timerController.Start();
+
+    auto timer = timerController.CreateAndScheduleTimer(1000ms, [](){
+        static int expCounter = 0;
+        printf("Expired: %d\n", expCounter);
+        expCounter++;
     });
+
+    printf("Waiting for expiry\n");
+    while(!timer->HasExpired()) {
+        std::this_thread::yield();
+    }
+    std::this_thread::yield();
+
+    printf("Restarting\n");
+    timerController.RestartTimer(timer);
+
+    printf("Waiting for expiry again\n");
     while(!timer->HasExpired()) {
         std::this_thread::yield();
     }

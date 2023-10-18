e97b24b36cdee4aee89588fd1920e4427ae00a1e
gnilk <1664069+gnilk@users.noreply.github.com>
2023-10-18 19:27:23 +0200

    Updated to do

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
  * TO-DO List
  * + Spotted another exception related to timers - but I think that was CPP-mode line bug
  * + Vertical navigation yet-again is acting strange on clipping when at the end of a file
- * - Delete a selection which start's at X (col) > 0 and ends on another line with col == 0 will remove one line too much
  * - Large(?) files issue, after searching for an item and jumping to next a couple of times - scrolling up doesn't properly reposition view (need to scroll down first)
  * - Delete some lines (upper 1/3 of file) and then page-down => segfault
  *   => Seen once??
@@ -59,6 +58,7 @@
  *   need to consider a solution for this...
  *
  * Done:
+ * ! Delete a selection which start's at X (col) > 0 and ends on another line with col == 0 will remove one line too much
  * ! Spotted exception when in an empty file typing a line (CPP) mode ending with {} and pressing enter
  *   [2023-10-18, gnilk] I think this was fixed by removing the use of iterators and instead working with index
  * ! Undo does almost work

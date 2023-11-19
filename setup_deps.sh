5c0569aae0d634c1eb7cc1ac5b37dc05a6d4542f
gnilk <1664069+gnilk@users.noreply.github.com>
2023-11-19 10:09:28 +0100

    FIX: Dukglue dependency now points to my fork

--- setup_deps.sh
+++ setup_deps.sh
@@ -5,19 +5,7 @@ git clone https://github.com/gnilk/dukglue ext/dukglue
 # git clone https://github.com/gnilk/logger ext/logger
 git clone https://github.com/gnilk/gnklog ext/gnklog
 
-# clone specifically branch 2.7.0 of duktape
+
 # NOTE: You need to run the configure script first!!!
 git clone https://github.com/nlohmann/json ext/json
 
-
-# git clone -b v2.7.0 https://github.com/svaarala/duktape ext/duktape-v2.7.0
-#
-# Note: Other dependencies you need to install include
-#
-#   - SDL3 or SDL2 (depending on which you prefer)
-#   - NCurses (the backend is still a hard requirement)
-#   - yaml-cpp, https://github.com/jbeder/yaml-cpp
-#   - nlohmann/json, https://github.com/nlohmann/json
-
-# This is added already directly to the repo
-# - stb, https://github.com/nothings/stb, ttf and rect_pack (added to my repo)

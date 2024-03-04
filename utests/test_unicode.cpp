541f45e2682b1f9f1f384047e1869a05ca2d25b9
gnilk <1664069+gnilk@users.noreply.github.com>
2024-03-04 10:54:15 +0100

    NEW: Unit test for UTF8 to UTF32 single code point converter

--- utests/test_unicode.cpp
+++ utests/test_unicode.cpp
@@ -4,6 +4,7 @@
 
 #include <testinterface.h>
 #include "Core/UnicodeHelper.h"
+#include "Core/HexDump.h"
 
 using namespace gedit;
 
@@ -15,7 +16,23 @@ DLL_EXPORT int test_unicode(ITesting *t) {
     return kTR_Pass;
 }
 DLL_EXPORT int test_unicode_convchar(ITesting *t) {
-    static uint8_t utf8chars[]={0x32,0x41,0x00};
-    printf("%s\n", utf8chars);
+    static uint8_t utf8chars[]={0x4d, 0xc3, 0xa4, 0x72, 0x00};
+
+    HexDump::ToConsole(utf8chars, sizeof(utf8chars));
+
+    std::u32string u32String;
+    int pos = 0;
+    while(utf8chars[pos] != 0) {
+        int numConsumed = UnicodeHelper::ConvertUTF8ToUTF32Char(u32String, &utf8chars[pos], sizeof(utf8chars) - pos);
+        TR_ASSERT(t, numConsumed >= 0);
+        printf("Consumed: %d\n", numConsumed);
+        pos += numConsumed;
+        if (pos >= sizeof(utf8chars)) {
+            break;
+        }
+    }
+    std::string utf8String;
+    TR_ASSERT(t, UnicodeHelper::ConvertUTF32ToUTF8String(utf8String, u32String));
+    printf("Converted back: %s\n", utf8String.c_str());
     return kTR_Pass;
 }

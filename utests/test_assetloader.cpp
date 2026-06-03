1192846cb85c136dcc6640ed20374c0380919dd8
gnilk <1664069+gnilk@users.noreply.github.com>
2026-06-03 09:12:15 +0200

    FIX: asset loader unit testing

--- utests/test_assetloader.cpp
+++ utests/test_assetloader.cpp
@@ -3,6 +3,7 @@
 //
 #include <testinterface.h>
 #include "Core/AssetLoaderBase.h"
+#include "Core/RuntimeConfig.h"
 
 using namespace gedit;
 
@@ -10,31 +11,27 @@ extern "C" {
 DLL_EXPORT int test_assetloader(ITesting *t);
 DLL_EXPORT int test_assetloader_load(ITesting *t);
 DLL_EXPORT int test_assetloader_loadne(ITesting *t);
-DLL_EXPORT int test_assetloader_loadtext(ITesting *t);
 }
 
 DLL_EXPORT int test_assetloader(ITesting *t) {
     return kTR_Pass;
 }
 
+// Load an asset - note, the asset comes from resources - this requires the 'Editor::Initialize' to have been called
+// which it is, through 'test_main'
 DLL_EXPORT int test_assetloader_load(ITesting *t) {
-    AssetLoaderBase assetLoaderBase;
-    auto asset = assetLoaderBase.LoadAsset("dummy.fil");
-    TR_ASSERT(t, asset != nullptr);
-    TR_ASSERT(t, asset->GetSize() != 0);
-    return kTR_Pass;
-}
-DLL_EXPORT int test_assetloader_loadtext(ITesting *t) {
-    AssetLoaderBase assetLoaderBase;
-    auto asset = assetLoaderBase.LoadAsset("dummy.fil");
+    auto assetLoader = RuntimeConfig::Instance().GetAssetLoader();
+    auto asset = assetLoader.LoadAsset("colors.json");
     TR_ASSERT(t, asset != nullptr);
     TR_ASSERT(t, asset->GetSize() != 0);
     return kTR_Pass;
 }
 
+
+// Load non-existing
 DLL_EXPORT int test_assetloader_loadne(ITesting *t) {
-    AssetLoaderBase assetLoaderBase;
-    auto asset = assetLoaderBase.LoadAsset("wefwef.wefwef");
+    auto assetLoader = RuntimeConfig::Instance().GetAssetLoader();
+    auto asset = assetLoader.LoadAsset("wefwef.wefwef");
     TR_ASSERT(t, asset == nullptr);
     return kTR_Pass;
 }
